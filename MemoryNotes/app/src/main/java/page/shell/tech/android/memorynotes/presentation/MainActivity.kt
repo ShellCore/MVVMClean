46e04cd05aee0557d8313925c9e86231a4d6d265
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-05-05 06:19:32 -0500

    Se agregaron las dependencias en el proyecto

--- MemoryNotes/app/src/main/java/page/shell/tech/android/memorynotes/presentation/MainActivity.kt
+++ MemoryNotes/app/src/main/java/page/shell/tech/android/memorynotes/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package page.shell.tech.android.memorynotes
+package page.shell.tech.android.memorynotes.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import page.shell.tech.android.memorynotes.R
 
 class MainActivity : AppCompatActivity() {
 
