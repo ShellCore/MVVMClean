947bcb0f90a2798fb782e3e0681066e27b59a13e
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-05-09 10:05:31 -0500

    Implementación de navegación entre las vistas.

--- MemoryNotes/app/src/main/java/page/shell/tech/android/memorynotes/presentation/ListFragment.kt
+++ MemoryNotes/app/src/main/java/page/shell/tech/android/memorynotes/presentation/ListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_list.*
 import page.shell.tech.android.memorynotes.R
 
 class ListFragment : Fragment() {
@@ -16,4 +18,16 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnAdd.setOnClickListener {
+            goToNoteDetails()
+        }
+    }
+
+    private fun goToNoteDetails(id: Long = 0L) {
+        val action = ListFragmentDirections.actionGoToNote(id)
+        Navigation.findNavController(recList).navigate(action)
+    }
 }
--- MemoryNotes/app/src/main/java/page/shell/tech/android/memorynotes/presentation/NoteFragment.kt
+++ MemoryNotes/app/src/main/java/page/shell/tech/android/memorynotes/presentation/NoteFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_note.*
 import page.shell.tech.android.memorynotes.R
 
 class NoteFragment : Fragment() {
@@ -16,4 +18,12 @@ class NoteFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_note, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnSave.setOnClickListener {
+            Navigation.findNavController(it)
+                .popBackStack()
+        }
+    }
 }
