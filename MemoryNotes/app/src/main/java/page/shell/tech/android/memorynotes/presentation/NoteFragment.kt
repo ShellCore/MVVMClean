5a927bc097daf0710b7480dac3fe397c8790d46a
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-05-09 09:50:33 -0500

    Creación de la vista de detalle.

---
+++ MemoryNotes/app/src/main/java/page/shell/tech/android/memorynotes/presentation/NoteFragment.kt
@@ -0,0 +1,19 @@
+package page.shell.tech.android.memorynotes.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import page.shell.tech.android.memorynotes.R
+
+class NoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+}
